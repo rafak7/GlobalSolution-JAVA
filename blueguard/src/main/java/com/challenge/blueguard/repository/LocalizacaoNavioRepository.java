5317dd4e703758498f640770d8fd0ea9aa340d03
Rafael Lino <126628341+rafak7@users.noreply.github.com>
2024-06-06 22:56:13 -0300

    Delete blueguard directory

--- blueguard/src/main/java/com/challenge/blueguard/repository/LocalizacaoNavioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.challenge.blueguard.repository;
-
-import com.challenge.blueguard.entity.LocalizacaoNavio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocalizacaoNavioRepository extends JpaRepository<LocalizacaoNavio, Long> {
-}
