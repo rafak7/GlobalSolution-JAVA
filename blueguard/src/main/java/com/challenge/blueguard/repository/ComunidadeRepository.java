9990f0b5d917660b0deff5c20113c9aee5e9dcdc
Rafael Lino <126628341+rafak7@users.noreply.github.com>
2024-06-07 19:40:17 -0300

    Delete blueguard directory

--- blueguard/src/main/java/com/challenge/blueguard/repository/ComunidadeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.challenge.blueguard.repository;
-
-import com.challenge.blueguard.entity.Comunidade;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ComunidadeRepository  extends JpaRepository<Comunidade, Long> {
-}
--- blueguard/src/main/java/com/challenge/blueguard/repository/MarcacoesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.challenge.blueguard.repository;
-
-import com.challenge.blueguard.entity.Marcacoes;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarcacoesRepository extends JpaRepository<Marcacoes, Long> {
-}
